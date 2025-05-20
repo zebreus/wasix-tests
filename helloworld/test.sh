599f64927904df5b92a99e764452eee87d9583ec
Zebreus <zebreus@zebre.us>
2025-05-20 17:49:38 +0200

    Use clang linker wrapper

--- helloworld/test.sh
+++ helloworld/test.sh
@@ -5,7 +5,9 @@ source ../lib/assert.sh
 
 make helloworld.wasm
 
-wasmer run helloworld.wasm > stdout.log 2> stderr.log
+if wasmer run --mapdir /lib:$(pwd) helloworld.wasm > stdout.log 2> stderr.log ; then : ; else
+    assert_eq 0 $? "wasmer run failed: $(cat stderr.log)"
+fi
 
 assert_eq "Hello, World!" "$(cat stdout.log)" "stdout did not match expected value"
 assert_eq "" "$(cat stderr.log)" "stderr did not match expected value"
--- simple-shared-lib/test.sh
+++ simple-shared-lib/test.sh
@@ -3,9 +3,12 @@ set -e
 cd "$(dirname "$0")"
 source ../lib/assert.sh
 
-make main.wasm libside.so
+make libside.so
+make main.wasm
 
-wasmer run --mapdir /lib:$(pwd) main.wasm > stdout.log 2> stderr.log
+if wasmer run --mapdir /lib:$(pwd) main.wasm > stdout.log 2> stderr.log ; then : ; else
+    assert_eq 0 $? "wasmer run failed: $(cat stderr.log)"
+fi
 
 assert_eq "The shared library returned: 42" "$(cat stdout.log)" "stdout did not match expected value"
 assert_eq "" "$(cat stderr.log)" "stderr did not match expected value"
