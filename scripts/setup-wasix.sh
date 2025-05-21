6f0345844ff99d259793df981dc0a33a3b71faaa
Zebreus <zebreus@zebre.us>
2025-05-21 23:49:34 +0200

    Silence setup-wasix.sh

--- scripts/setup-wasix.sh
+++ scripts/setup-wasix.sh
@@ -9,20 +9,20 @@ if [ -d "/wasix-sysroot" ]; then
     exit 0
 fi
 
-curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash  > /dev/null
 cargo binstall -y wasm-tools
 
-sudo apt update
-sudo apt install -y clang-19 llvm-19 llvm-19-dev emscripten nodejs binaryen wabt libpolly-19-dev lld-19 pkg-config libtool unzip
+sudo apt update > /dev/null
+sudo apt install -y clang-19 llvm-19 llvm-19-dev emscripten nodejs binaryen wabt libpolly-19-dev lld-19 pkg-config libtool unzip  > /dev/null
 
-wget http://0x0.st/8wwU.zip
-unzip 8wwU.zip
+wget http://0x0.st/8wwU.zip > /dev/null
+unzip 8wwU.zip > /dev/null
 chmod a+x wasmer
 sudo mv wasmer /usr/bin/wasmer
 rm 8wwU.zip
 
-wget http://0x0.st/8ww5.zip
-unzip 8ww5.zip
+wget http://0x0.st/8ww5.zip > /dev/null
+unzip 8ww5.zip > /dev/null
 sudo mv sysroot /wasix-sysroot
 rm 8ww5.zip
 
