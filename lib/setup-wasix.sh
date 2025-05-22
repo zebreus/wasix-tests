11954f5919f17998f001bb9dba215b42a1fa545b
Zebreus <zebreus@zebre.us>
2025-05-22 14:10:46 +0200

    Fix CI

--- lib/setup-wasix.sh
+++ lib/setup-wasix.sh
@@ -13,7 +13,22 @@ curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-
 cargo binstall -y wasm-tools
 
 sudo apt update > /dev/null
-sudo apt install -y clang-19 llvm-19 llvm-19-dev emscripten nodejs binaryen wabt libpolly-19-dev lld-19 pkg-config libtool unzip  > /dev/null
+sudo apt install -y clang-19 llvm-19 llvm-19-dev nodejs binaryen wabt libpolly-19-dev lld-19 pkg-config libtool unzip  > /dev/null
+
+git clone --depth=1 https://github.com/emscripten-core/emsdk.git > /dev/null
+cd emsdk
+./emsdk install 3.1.69 > /dev/null
+./emsdk activate 3.1.69
+source "$(pwd)/emsdk_env.sh"
+echo 'source "'$(pwd)'/emsdk_env.sh"' >> $HOME/.bash_profile
+echo 'source "'$(pwd)'/emsdk_env.sh"' >> $HOME/.bashrc
+echo 'source "'$(pwd)'/emsdk_env.sh"' >> $HOME/.profile
+if test -n "${GITHUB_PATH:-}" ; then
+  echo "$(pwd)" >> $GITHUB_PATH
+  echo "$(pwd)/upstream/emscripten" >> $GITHUB_PATH
+fi
+
+cd ..
 
 wget -q http://0x0.st/8wwU.zip > /dev/null
 unzip 8wwU.zip > /dev/null
--- lib/summarize-test-results-for-ci.sh
+++ lib/summarize-test-results-for-ci.sh
@@ -1,6 +1,6 @@
 summary=$(tail -n 1 test.log | sed -r 's/\x1b\[[0-9;]*m//g')
 failed="$(grep -Po "failed: [a-zA-Z-]+ [(][a-zA-Z-]+[)]" test.log | sed -r 's/failed: /- /g')"
-failed_details="$(cat test.log | sed -E 's/^/  /g' |  sed -E 's@  --- ([a-zA-Z-]+ [(][a-zA-Z-]+[)]) ---@* \1 <details><summary>Output</summary>\n  ```@' | sed -E 's@  --------@  ```\n</details>@' | sed -n '/^  failed:.*/!p' | sed -n '/^  ✅/!p')"
+failed_details="$(cat test.log | sed -E 's/^/  /g' |  sed -E 's@  --- ([a-zA-Z-]+ [(][a-zA-Z-]+[)]) ---@* \1 <details><summary>Output</summary>\n  ```@' | sed -E 's@  --------@  ```\n  </details>@' | sed -n '/^  failed:.*/!p' | sed -n '/^  ✅/!p')"
 numbers=($(echo "$summary" | grep -oE '[0-9]+'))
 pass_count=${numbers[0]:-0}
 fail_count=${numbers[1]:-0}
