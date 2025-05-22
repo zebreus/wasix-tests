18006468d4d7aab320fb8d4951937e7dae064173
Zebreus <zebreus@zebre.us>
2025-05-22 14:01:30 +0200

    Add more debugging info for tests

--- lib/test-utils.sh
+++ lib/test-utils.sh
@@ -3,11 +3,13 @@ set -euo pipefail
 
 # Run the specified file using $RUNNER (or use a native runner if not set)
 run() {
+    set -x
     RUNNER=${RUNNER:-../lib/wrappers/native-clang-runner}
     local executable="$1"
     shift
     if ! $RUNNER "./$executable" "$@" > stdout.log 2> stderr.log; then
         assert_eq 0 "$?" "run failed: $(cat stderr.log)"
     fi
+    set +x
 }
 
