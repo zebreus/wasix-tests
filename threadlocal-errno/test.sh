c29f373e73dc2bbda3edf47d8e6aff494a5f40c7
Zebreus <zebreus@zebre.us>
2025-05-23 01:38:08 +0200

    Add threadlocal errno test

---
+++ threadlocal-errno/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# This test should pass with both wasix-clang and native gcc/clang
+set -e
+cd "$(dirname "$0")"
+source ../lib/assert.sh
+source ../lib/test-utils.sh
+
+make main
+run main
+
+expected=$'main errno 1\nthread 0 errno 100\nthread 1 errno 101\nthread 2 errno 102\nthread 3 errno 103'
+assert_eq "$expected" "$(cat stdout.log)" "stdout did not match expected value"
+assert_eq "" "$(cat stderr.log)" "stderr did not match expected value"
