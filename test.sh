63068c33542cacbe751d95cc6336ae258c627e89
Zebreus <zebreus@zebre.us>
2025-05-23 13:54:08 +0200

    Improve test script

--- test.sh
+++ test.sh
@@ -7,21 +7,56 @@ cd "$(dirname "$0")"
 export TERM="${TERM:-xterm-256color}"
 export WASIX_SYSROOT="${WASIX_SYSROOT:-/wasix-sysroot}"
 
-disabled_tests=("minimal-threadlocal" "extern-threadlocal-nopic")
+available_tools=(
+    "wasix-clang"
+    "emscripten"
+    "native-clang"
+    "native-gcc"
+)
+selected_tools=()
 
-# Gather list of enabled tests to calculate total progress steps
+disabled_tests=("minimal-threadlocal" "extern-threadlocal-nopic")
+available_tests=()
 enabled_tests=()
+
+selected_tests=( )
+use_only_selected_tests=false
+
 for t in ./*/test.sh; do
     dir=$(dirname "$t")
     name=$(basename "$dir")
+    available_tests+=("$name")
+done
+
+for name in "${available_tests[@]}"; do
     if [[ " ${disabled_tests[@]} " =~ " ${name} " ]]; then
         continue
     fi
     enabled_tests+=("$name")
 done
 
+for test in "$@"; do
+    if [[ ${available_tools[@]} =~ $test  ]] ; then 
+        selected_tools+=("$test")
+        continue
+    fi
+    if [[ " ${available_tests[@]} " =~ " ${test} " ]]; then
+        selected_tests+=("$test")
+        continue
+    fi
+    echo "$test is not a test directory or a tool"
+done
+
+# use all tools if no specific tools are selected
+if [[ ${#selected_tools[@]} -eq 0 ]]; then
+    selected_tools=("${available_tools[@]}")
+fi
+if [[ ${#selected_tests[@]} -eq 0 ]]; then
+    selected_tests=("${enabled_tests[@]}")
+fi
+
 # Number of test runs (per toolchain)
-total_steps=$(( ${#enabled_tests[@]} * 4 ))
+total_steps=$(( ${#selected_tests[@]} * ${#selected_tools[@]} ))
 current_step=0
 
 draw_progress() {
@@ -29,8 +64,12 @@ draw_progress() {
     local filled=$(( progress * width / total ))
     local empty=$(( width - filled ))
     printf "\r[" >&2
-    printf '%0.s#' $(seq 1 $filled) >&2
-    printf '%0.s ' $(seq 1 $empty) >&2
+    for i in $(seq 1 $((filled))); do
+        printf '#' >&2
+    done
+    for i in $(seq 1 $((empty))); do
+        printf ' ' >&2
+    done
     printf "] %d/%d" "$progress" "$total" >&2
 }
 
@@ -39,17 +78,14 @@ failed=0
 declare -a failed_tests
 declare -a failed_logs
 
-for tool in wasix-clang emscripten native-clang native-gcc; do
+for tool in "${selected_tools[@]}"; do
     export PATH="$(pwd)/lib/wrappers:$PATH"
     export CC="${tool}" CXX="${tool}++" RUNNER="${tool}-runner"
 
-    for testfile in ./*/test.sh; do
+    for test in "${selected_tests[@]}"; do
+        testfile="./$test/test.sh"
         testdir=$(dirname "$testfile")
         testname=$(basename "$testdir")
-
-        if [[ " ${disabled_tests[@]} " =~ " ${testname} " ]]; then
-            continue
-        fi
 
         ((current_step+=1))
         draw_progress "$current_step" "$total_steps"
