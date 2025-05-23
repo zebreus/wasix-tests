143581cac6700d5f3f73cb0ba13602a8f18cf984
Zebreus <zebreus@zebre.us>
2025-05-23 13:54:28 +0200

    Add a funky problem

---
+++ funky-problem/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd "$(dirname "$0")"
+
+source ../lib/assert.sh
+source ../lib/test-utils.sh
+
+make libside.so main
+run main
+
+assert_eq ".Nothing weird happened" "$(cat stdout.log)" "stdout did not match expected value"
+assert_eq "" "$(cat stderr.log)" "stderr did not match expected value"
+
