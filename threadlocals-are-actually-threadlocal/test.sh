e2a3ec1c7279585554a1e570fa177495bee70c97
Zebreus <zebreus@zebre.us>
2025-05-28 13:44:26 +0200

    Added a test to verify that threadlocals are actually threadlocal

---
+++ threadlocals-are-actually-threadlocal/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd "$(dirname "$0")"
+source ../lib/assert.sh
+source ../lib/test-utils.sh
+
+make libside.so main
+run main
+
+assert_eq "value=10 value=11 value=10 value=12 " "$(cat stdout.log)" "stdout did not match expected value"
+assert_eq "" "$(cat stderr.log)" "stderr did not match expected value"
+
