c183810e643607038b60f1e70fb77b7b1a2e1659
Zebreus <zebreus@zebre.us>
2025-05-30 10:20:51 +0200

    Add minimal testcase for threadlocals from other modules

---
+++ minimal-threadlocals/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd "$(dirname "$0")"
+source ../lib/assert.sh
+source ../lib/test-utils.sh
+
+make libside.so main
+run main
+
+assert_eq "10 20" "$(cat stdout.log)" "stdout did not match expected value"
+assert_eq "" "$(cat stderr.log)" "stderr did not match expected value"
